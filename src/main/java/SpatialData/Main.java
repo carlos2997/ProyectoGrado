45d932616e300c4a32b04c7fd327291ae9a55c25
Carlos Alberto Ramirez Otero <carlosramirez-2997@hotmail.com>
2018-02-21 08:45:40 -0500

    Config host ECI

--- src/main/java/SpatialData/Main.java
+++ src/main/java/SpatialData/Main.java
@@ -1,11 +1,13 @@
 
 package SpatialData;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.util.JSON;
 import org.json.JSONObject;
 
@@ -19,42 +21,53 @@ public class Main {
     static SpatialQueryMongo sqMongo;
     
     public static void main(String[] args) throws Throwable{
-        MongoClient mongoClient = new MongoClient();
+        
+        MongoClientURI uri = new MongoClientURI("mongodb://10.8.0.23:27017");
+        
+        MongoClient mongoClient = new MongoClient(uri);
 
-        myDatabase = mongoClient.getDB("prueba");
+        myDatabase = mongoClient.getDB("test");
         
-        sqMongo = new SpatialQueryMongo();
+        DBCollection collection = myDatabase.getCollection("hola");
         
-        DBCollection collection = myDatabase.getCollection("Polygons");
+        BasicDBObject document = new BasicDBObject();
+        document.put("database", "mkyongDB");
+        document.put("table", "hosting");
         
-        double[] coordenadasPunto =  {4.886684417724609,52.36627132364143};
+        collection.insert(document);
         
-        String nameLand = null;
+//        sqMongo = new SpatialQueryMongo();
         
-        DBCursor cursor = collection.find(sqMongo.queryPointInPolygon(coordenadasPunto));
-        try {
-            while(cursor.hasNext()) {
-                DBObject cur = cursor.next();
-                System.out.println(cur);
-                JSONObject output = new JSONObject(JSON.serialize(cur));
-                nameLand = output.getJSONObject("polygons").getString("nameField");
-            }
-        } finally {
-           cursor.close();
-        }
-        
-        DBCollection collection1 = myDatabase.getCollection("fs.files");
-        
-        DBCursor cursor1 = collection1.find(sqMongo.queryPolygonPictures(nameLand));
-        try {
-            while(cursor1.hasNext()) {
-                DBObject cur1 = cursor1.next();
-                System.out.println(cur1);
-            }
-        } finally {
-           cursor1.close();
-        }
-        
+//        
+//        
+//        double[] coordenadasPunto =  {4.886684417724609,52.36627132364143};
+//        
+//        String nameLand = null;
+//        
+//        DBCursor cursor = collection.find(sqMongo.queryPointInPolygon(coordenadasPunto));
+//        try {
+//            while(cursor.hasNext()) {
+//                DBObject cur = cursor.next();
+//                System.out.println(cur);
+//                JSONObject output = new JSONObject(JSON.serialize(cur));
+//                nameLand = output.getJSONObject("polygons").getString("nameField");
+//            }
+//        } finally {
+//           cursor.close();
+//        }
+//        
+//        DBCollection collection1 = myDatabase.getCollection("fs.files");
+//        
+//        DBCursor cursor1 = collection1.find(sqMongo.queryPolygonPictures(nameLand));
+//        try {
+//            while(cursor1.hasNext()) {
+//                DBObject cur1 = cursor1.next();
+//                System.out.println(cur1);
+//            }
+//        } finally {
+//           cursor1.close();
+//        }
+//        
         
     }
 }
